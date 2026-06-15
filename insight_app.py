0ef10c6aa991e15a07bfd37e7abd9109347a3a11
ohusain <omar.latif.husain@gmail.com>
2026-06-15 12:42:42 -0400

    Update insight_app.py

--- insight_app.py
+++ insight_app.py
@@ -85,9 +85,9 @@ kmeans = KMeans(n_clusters=3, random_state=42)
 df["segment"] = kmeans.fit_predict(features) 
 segment_map = {   
   
-"High Value", 
-"Traveler", 
-    2: "     Shopper" 
+0: "High Value", 
+1: "Traveler", 
+2: "Shopper" 
 } 
  
 # -------------------------- 
